31c332d37dd684df4d0d6a50266ccf7f53f0d9e3
DaniellaOhenewaaAsumadu <you@example.com>
2024-11-29 11:07:26 +0000

    added a few changes

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let x=prompt("give me a number");
+console.log(typeof(x));
+x=Number(x);
+console.log(typeof(x));
+let result=(x+15);
+let remainder=result%2;
+// if(condition){}
+if(remainder==1){
+    alert("winner")
+}else{"looser"};
+// if(typeof(x)=="Number"&& remainder==1){
+//     alert("winner")}
+//     else{"looser"};
+
+
